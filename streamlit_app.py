67d4307d1feae9c6645394a677c3fdb537a5d331
Deepthisri2230 <153616455+Deepthisri2230@users.noreply.github.com>
2023-12-18 20:07:32 +0530

    Update streamlit_app.py
    Added a input text line as fruit_choice
    added a fruit_choice results in response line

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,14 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Show JSON Response to normalized way
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Show the results in table form 
 streamlit.dataframe(fruityvice_normalized)
+
