24da2d895b5562f273149dfff5ebbe81d242ea6c
Deepthisri2230 <153616455+Deepthisri2230@users.noreply.github.com>
2023-12-22 13:12:01 +0530

    Update streamlit_app.py
    Updated code with try, else, except statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
 from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
@@ -25,17 +26,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice
+streamlit.error("Please select a fruit to get information")
+else
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Show JSON Response to normalized way
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Show the results in table form 
 streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+streamlit.error()
+
+
+
+
 
 # dont run anything past herewhile we troubleshoot
 streamlit.stop()
